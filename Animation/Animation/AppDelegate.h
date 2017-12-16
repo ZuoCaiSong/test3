eb0ea07016c1b8cdc8fde62e38bdd93d87a38672
谭登 <tandeng@tandengdeMacBook-Pro.local>
2017-12-16 16:35:44 +0800

    core Animation

---
+++ Animation/Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animation
+//
+//  Created by 谭登 on 2017/12/16.
+//  Copyright © 2017年 谭登. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
