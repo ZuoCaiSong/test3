9b83026e18985d8a34f0633ee903d6aa29b5162a
谭登 <tandeng@tandengdeMacBook-Pro.local>
2017-12-17 01:22:09 +0800

    sdsdds232

---
+++ Animation/Animation/test2View.h
@@ -0,0 +1,13 @@
+//
+//  test2View.h
+//  Animation
+//
+//  Created by 谭登 on 2017/12/17.
+//  Copyright © 2017年 谭登. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test2View : UIView
+
+@end
---
+++ Animation/Animation/test2View.m
@@ -0,0 +1,21 @@
+//
+//  test2View.m
+//  Animation
+//
+//  Created by 谭登 on 2017/12/17.
+//  Copyright © 2017年 谭登. All rights reserved.
+//
+
+#import "test2View.h"
+
+@implementation test2View
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
